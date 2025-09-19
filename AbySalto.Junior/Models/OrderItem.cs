218f8df44dfd05cc2a09a6d3dcf6e47baa4b28ae
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2025-09-19 09:37:06 +0200

    Add models for orders and there items

---
+++ AbySalto.Junior/Models/OrderItem.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AbySalto.Junior.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public required string Name { get; set; }
+
+        [Required, Range(1, 1000)]
+        public int Quantity { get; set; }
+
+        [Required, DataType(DataType.Currency), Range(0.01, 100000)]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int OrderId { get; set; }
+        [Required]
+        public required Order Order { get; set; }
+    }
+}
